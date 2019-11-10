194869e5312752e48250a5d1693f099e1359395e
Tiago M. Nunes <37198181+tiagomnunes89@users.noreply.github.com>
2019-11-10 20:34:15 -0300

    start reactive project

---
+++ app/src/main/java/br/com/courserxjavarxandroid/MainActivity.java
@@ -0,0 +1,25 @@
+package br.com.courserxjavarxandroid;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button btRX00Intro;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        setUpView();
+    }
+
+    private void setUpView() {
+        btRX00Intro = findViewById(R.id.btRX00Intro);
+        btRX00Intro.setOnClickListener(v -> startActivity(new Intent(getApplicationContext(),Rx00IntroActivity.class)));
+    }
+}
---
+++ app/src/main/java/br/com/courserxjavarxandroid/Rx00IntroActivity.java
@@ -0,0 +1,14 @@
+package br.com.courserxjavarxandroid;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Rx00IntroActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rx00_intro);
+    }
+}
