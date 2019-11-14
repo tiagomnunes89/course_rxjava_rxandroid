cd74601f2227f64f8ecb9da1632ccdab2577c429
Tiago M. Nunes <37198181+tiagomnunes89@users.noreply.github.com>
2019-11-14 14:27:34 -0300

    First subscription

--- app/src/main/java/br/com/courserxjavarxandroid/Rx00IntroActivity.java
+++ app/src/main/java/br/com/courserxjavarxandroid/Rx00IntroActivity.java
@@ -3,6 +3,11 @@ package br.com.courserxjavarxandroid;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import io.reactivex.Observable;
+import io.reactivex.Observer;
+import io.reactivex.disposables.Disposable;
 
 public class Rx00IntroActivity extends AppCompatActivity {
 
@@ -10,5 +15,32 @@ public class Rx00IntroActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_rx00_intro);
+
+        Observable<String> numbersObservable =
+                Observable.just("1", "2", "3", "4", "5", "6", "7", "8", "9", "10");
+
+        Observer<String> numbersObserver = new Observer<String>() {
+            @Override
+            public void onSubscribe(Disposable d) {
+                Log.d("TAG1", "onSubscribe" + " Thread: " + Thread.currentThread().getName());
+            }
+
+            @Override
+            public void onNext(String numero) {
+                Log.d("TAG1", "onNext - Number: " + numero + " Thread: " + Thread.currentThread().getName());
+            }
+
+            @Override
+            public void onError(Throwable e) {
+                Log.d("TAG1", "onError" + " Thread: " + Thread.currentThread().getName());
+            }
+
+            @Override
+            public void onComplete() {
+                Log.d("TAG1", "onComplete: All data has been issued" + " Thread: " + Thread.currentThread().getName());
+            }
+        };
+
+        numbersObservable.subscribe(numbersObserver);
     }
 }
